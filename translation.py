16843cfbc18a88d16eeca11caf7ff450dee9b3da
facanhe <1841634171@qq.com>
2023-02-15 09:30:00 +0800

    translation

--- translation.py
+++ translation.py
@@ -1,12 +1,13 @@
-import translators as ts
+import numpy as np
+import translators.server as ts
 import pandas as pd
 from multiprocessing import Pool
 from tqdm import tqdm
 
 LANG = 'fr'
-API = 'baidu'
+API = 'google'
 
-lang_dict = {'Chinese': 'cht', 'French': 'fra', 'Italian': 'it', 'Portuguese': 'pt', 'Spanish': 'spa', 'English': 'en'}
+lang_dict = {'Chinese': 'cht', 'French': 'fr', 'Italian': 'it', 'Portuguese': 'pt', 'Spanish': 'spa', 'English': 'en'}
 
 
 def translator_constructor(api):
@@ -29,11 +30,16 @@ def translator_constructor(api):
 
 
 def translate(x):
+    #print(x[2])
+    print([x[0], x[1],translator_constructor(API)(x[2], 'fr', 'en',if_ignore_limit_of_length=True),translator_constructor(API)(x[3], 'fr', 'en',if_ignore_limit_of_length=True)])
+    #return[x[0], x[1],translator_constructor(API)(x[2], 'en', 'fr',if_ignore_limit_of_length=True),translator_constructor(API)(x[3], 'en', 'fr',if_ignore_limit_of_length=True)]
     # return [translator_constructor(API)(x[0], lang_dict[x[2]], 'en'), x[1], x[2]]
     try:
-        return [translator_constructor(API)(x[0], lang_dict[x[2]], 'en'), x[1], x[2]]
+        print([x[0], x[1],translator_constructor(API)(x[2], 'fr', 'en',if_ignore_limit_of_length=True),translator_constructor(API)(x[3], 'fr', 'en',if_ignore_limit_of_length=True)])
+        return[x[0], x[1],translator_constructor(API)(x[2], 'fr', 'en',if_ignore_limit_of_length=True),translator_constructor(API)(x[3], 'fr', 'en',if_ignore_limit_of_length=True)]
+        print(translator_constructor(API)(x[2],x[3], 'fra'))
     except:
-        return [x[0], x[1], x[2]]
+        return [x[0], x[1], x[2],x[3]]
 
 
 def imap_unordered_bar(func, args, n_processes: int = 48):
@@ -50,10 +56,14 @@ def imap_unordered_bar(func, args, n_processes: int = 48):
 
 
 def main():
-    df = pd.read_csv('./trainingdata/traindata.csv', header=0, sep=',', quoting=2)
+    df = pd.read_csv('./trainingdata/translated_train.csv',dtype={'Label': np.int})
+    df1=pd.read_csv('./trainingdata/traindata.csv',dtype={'Label': np.int})
     # tqdm.pandas('Translation progress')
-    df[['Statement','Premise']] = imap_unordered_bar(translate, df[['Statement', 'Premise']].values)
-    df.to_csv(f'./trainingdata/translated_train.csv', index=False)
+    df[['uuid','Label','Statement','Premise']] = imap_unordered_bar(translate, df[['uuid','Label','Statement', 'Premise']].values)
+    file=[df1,df]
+    train=pd.concat(file)
+    train.to_csv(f'./trainingdata/translated_traindata.csv', index=False)
+    #df.to_csv(f'./trainingdata/translated_train.csv', index=False)
 
 
 if __name__ == '__main__':
